9139db9a1e89149a2031f4e5b24a8f1d51f3b382
Aravind104 <ontagodiaravind@gmail.com>
2023-06-01 14:20:58 +0530

    add reducer function

---
+++ src/reducer.js
@@ -0,0 +1,29 @@
+export const initialState = {
+  todos: [],
+};
+
+export const todoReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        ...state,
+        todos: [...state.todos, action.payload],
+      };
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload
+            ? { ...todo, completed: !todo.completed }
+            : todo
+        ),
+      };
+    case 'DELETE_TODO':
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
